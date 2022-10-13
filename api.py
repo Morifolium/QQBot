59f594ae9abd943047ee10814422fa682e2b8d81
new <1054694788@qq.com>
2022-10-13 15:36:31 +0800

    new version

--- api.py
+++ api.py
@@ -1,3 +1,4 @@
+from cgitb import small
 from email import message
 import json
 
@@ -10,10 +11,44 @@ import random
 
 
 def keyword(message, uid, gid=None):
+    if message[0:2] == "色图":
+        return setu(uid, gid, message[2:])
+        pass
+    elif message[0:2] == "萝莉":
+        return loli(uid, gid)
+    """
     if message[0:3] == "余悉越":
         return yxy(uid, gid)
     if message[0:3] == "陈金宝":
         return cjb(uid, gid)
+    """
+
+
+def setu(uid, gid, message):
+    if gid != None:
+        message = message.split(" ")
+        tag = []
+        for i in message:
+            if len(i) > 0:
+                tag.append(i)
+        v2_url = "https://api.lolicon.app/setu/v2"
+        setu_json = requests.post(url=v2_url, data={"tag": tag})
+        setu_url = setu_json.json()["data"][0]["urls"]["original"]
+        requests.post(
+            url="http://127.0.0.1:5700/send_group_msg",
+            data={"group_id": gid, "message": r"[CQ:image,file=" + setu_url + r"]"},
+        )
+
+
+def loli(uid, gid):
+    if gid != None:
+        url = "https://api.lolicon.app/setu/v2?tag=萝莉&size=small"
+        text = requests.get(url)
+        setu = text.json()["data"][0]["urls"]["small"]
+        requests.post(
+            url="http://127.0.0.1:5700/send_group_msg",
+            data={"group_id": gid, "message": r"[CQ:image,file=" + setu + r"]"},
+        )
 
 
 def yxy(uid, gid):
